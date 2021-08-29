1a058e5bb432de8fef1dd1485471693ba0217e79
varshini <dreamvarshini@gmail.com>
2021-08-29 23:59:16 +0530

    ADDED:welcome message displayed

---
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -0,0 +1,16 @@
+package com.bridgelabz.maximumproblem;
+
+public class MaximumProblem 
+{
+
+
+	
+
+	public static void main(String[] args) 
+	{
+
+		System.out.println("------------welcome to maximum problem program------------");
+		
+	}
+
+}
