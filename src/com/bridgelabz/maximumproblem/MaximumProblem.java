3d5eb4c0e61eeedc29f82318feccd6e836222b26
varshini <dreamvarshini@gmail.com>
2021-08-30 00:32:06 +0530

    REFACTORED:used one generic method to find maximum

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -2,97 +2,35 @@ package com.bridgelabz.maximumproblem;
 
 public class MaximumProblem 
 {
-	public static void findMaximum(Integer integerNumber1,Integer integerNumber2,Integer integerNumber3)
-	{
-		if(integerNumber1.compareTo(integerNumber2)>0)
-		{
-			if(integerNumber1.compareTo(integerNumber3)>0)
-			{
-				System.out.println("maximum of three integers is "+integerNumber1);
-				
-			}
-			else
-			{
-				System.out.println("maximum of three integers is "+integerNumber3);
-			}
-			
-			
-		}
-		else
-		{
-			if(integerNumber2.compareTo(integerNumber3)>0)
-			{
-				System.out.println("maximum of three integers is "+integerNumber2);
-
-			}
-			else
-			{
-				System.out.println("maximum of three integers is "+integerNumber3);
-
-			}
-			
-		}
-	}
-		
-	public static void findMaximum(Float floatNumber1,Float floatNumber2,Float floatNumber3)
-	{
-		if(floatNumber1.compareTo(floatNumber2)>0)
-		{
-			if(floatNumber1.compareTo(floatNumber3)>0)
-			{
-				System.out.println("maximum of three float is "+floatNumber1);
-
-			}
-			else
-			{
-				System.out.println("maximum of three float is "+floatNumber3);
-			}
-
-
-		}
-		else
-		{
-			if(floatNumber2.compareTo(floatNumber3)>0)
-			{
-				System.out.println("maximum of three float is "+floatNumber2);
-
-			}
-			else
-			{
-				System.out.println("maximum of three float is "+floatNumber3);
-
-			}
-
-		}
-
-	}
 	
-	public static void findMaximum(String string1,String string2,String string3)
+	
+	
+	public static <E extends Comparable<E>> void findMaximum(E firstInput,E secondInput,E thirdInput)
 	{
-		if(string1.compareTo(string2)>0)
+		if(firstInput.compareTo(secondInput)>0)
 		{
-			if(string1.compareTo(string3)>0)
+			if(firstInput.compareTo(thirdInput)>0)
 			{
-				System.out.println("maximum of three string is "+string1);
+				System.out.println("maximum of three  is "+firstInput);
 
 			}
 			else
 			{
-				System.out.println("maximum of three string is "+string3);
+				System.out.println("maximum of three  is "+thirdInput);
 			}
 
 
 		}
 		else
 		{
-			if(string2.compareTo(string3)>0)
+			if(secondInput.compareTo(thirdInput)>0)
 			{
-				System.out.println("maximum of three string is "+string2);
+				System.out.println("maximum of three  is "+secondInput);
 
 			}
 			else
 			{
-				System.out.println("maximum of three string is "+string3);
+				System.out.println("maximum of three  is "+thirdInput);
 
 			}
 
@@ -102,8 +40,6 @@ public class MaximumProblem
 	
 	
 
-
-	
 
 	public static void main(String[] args) 
 	{
