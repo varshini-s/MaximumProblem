c95addb75bcfa5df4ae7aecedf62bf09c9cd11c1
varshini <dreamvarshini@gmail.com>
2021-08-30 00:03:04 +0530

    ADDED:method to find maximum of three integers

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -2,6 +2,41 @@ package com.bridgelabz.maximumproblem;
 
 public class MaximumProblem 
 {
+	public static void findMaximum(Integer integerNumber1,Integer integerNumber2,Integer integerNumber3)
+	{
+		if(integerNumber1.compareTo(integerNumber2)>0)
+		{
+			if(integerNumber1.compareTo(integerNumber3)>0)
+			{
+				System.out.println("maximum of three integers is "+integerNumber1);
+				
+			}
+			else
+			{
+				System.out.println("maximum of three integers is "+integerNumber3);
+			}
+			
+			
+		}
+		else
+		{
+			if(integerNumber2.compareTo(integerNumber3)>0)
+			{
+				System.out.println("maximum of three integers is "+integerNumber2);
+
+			}
+			else
+			{
+				System.out.println("maximum of three integers is "+integerNumber3);
+
+			}
+			
+		}
+
+
+
+
+	}
 
 
 	
@@ -10,6 +45,11 @@ public class MaximumProblem
 	{
 
 		System.out.println("------------welcome to maximum problem program------------");
+		Integer integerNumber1,integerNumber2,integerNumber3;
+		integerNumber1=10;
+		integerNumber2=5;
+		integerNumber3=9;
+		findMaximum(integerNumber1, integerNumber2, integerNumber3);
 		
 	}
 
