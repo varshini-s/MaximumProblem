ec5c6f62067acc45309d65b979d7ceae5fb5eef4
varshini <dreamvarshini@gmail.com>
2021-08-30 00:13:22 +0530

    ADDED:method to find maximum of three float numbers

--- src/com/bridgelabz/maximumproblem/MaximumProblem.java
+++ src/com/bridgelabz/maximumproblem/MaximumProblem.java
@@ -32,6 +32,38 @@ public class MaximumProblem
 			}
 			
 		}
+	}
+		
+		public static void findMaximum(Float floatNumber1,Float floatNumber2,Float floatNumber3)
+		{
+			if(floatNumber1.compareTo(floatNumber2)>0)
+			{
+				if(floatNumber1.compareTo(floatNumber3)>0)
+				{
+					System.out.println("maximum of three integers is "+floatNumber1);
+					
+				}
+				else
+				{
+					System.out.println("maximum of three integers is "+floatNumber3);
+				}
+				
+				
+			}
+			else
+			{
+				if(floatNumber2.compareTo(floatNumber3)>0)
+				{
+					System.out.println("maximum of three integers is "+floatNumber2);
+
+				}
+				else
+				{
+					System.out.println("maximum of three integers is "+floatNumber3);
+
+				}
+				
+			}
 
 
 
@@ -45,12 +77,11 @@ public class MaximumProblem
 	{
 
 		System.out.println("------------welcome to maximum problem program------------");
-		Integer integerNumber1,integerNumber2,integerNumber3;
-		integerNumber1=10;
-		integerNumber2=5;
-		integerNumber3=9;
+		Integer integerNumber1=1,integerNumber2=23,integerNumber3=4;
+		Float floatNumber1=1f,floatNumber2=7f,floatNumber3=4f;
 		findMaximum(integerNumber1, integerNumber2, integerNumber3);
-		
+		findMaximum(floatNumber1, floatNumber2, floatNumber3);
+
 	}
 
 }
